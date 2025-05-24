4dbabcff11f6366f4635aaa9d5bdb29d52fb9ca9
Whitebeard217 <54448227+Whitebeard217@users.noreply.github.com>
2025-05-23 22:36:33 -0400

    Delete GUAM_and_Aileron_Simulink directory
    I am uploading the new version instead of the old version to this branch

--- GUAM_and_Aileron_Simulink/runfg.bat
+++
@@ -1,7 +0,0 @@
-C:
-
-cd C:\Program Files\FlightGear 2020.3
-
-SET FG_ROOT=C:\Program Files\FlightGear 2020.3\data
-
-START .\\bin\fgfs.exe --fdm=null --native-fdm=socket,in,30,localhost,5502,udp   --prop:/sim/rendering/shaders/quality-level=0 --aircraft=c172p --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --airport=KJFK --runway=13L --altitude=1100 --heading=113 --offset-distance=0 --offset-azimuth=0  
--- GUAM_and_Aileron_Simulink/vehicles/Lift+Cruise/ClassDef/RefInputEnum.m
+++
@@ -1,8 +0,0 @@
-classdef RefInputEnum < Simulink.IntEnumType
-  enumeration
-    FOUR_RAMP(1)
-    ONE_RAMP(2)
-    TIMESERIES(3)
-    DEFAULT(4)
-  end
-end
